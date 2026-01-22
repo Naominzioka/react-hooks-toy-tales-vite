a72c910d8028d5b795af1135238677a710900500
Naomi Nzioka <nziokanaomi422@gmail.com>
2026-01-23 01:17:30 +0300

    display toys on page load

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+
+  const [toys, setToys] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetchToys()
+  }, [])
+
+
+  function fetchToys() {
+    //console.log('Fetching')
+    setLoading(true)
+    fetch("http://localhost:3001/toys")
+      .then(response => response.json())
+      .then(data => showToys(data))
+      .catch((err) => console.error(err))
+  }
+
+
+  function showToys(data) {
+    setLoading(false)
+    setToys([...data])
+  }
+
+  const contentView = (
+    <div id="toy-collection">{/* Render the collection of ToyCards */}
+      {toys.map(toy => <ToyCard
+        key={toy.id}
+        toy={toy}
+      />
+      )}
+    </div>
+  )
+
+  const loadingView = (
+    <p>Loading...</p>
+  )
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+   loading  ? loadingView : contentView
   );
 }
 
